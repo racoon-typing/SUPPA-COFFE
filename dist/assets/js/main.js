5753a56646235bf0add129914b78e686b2b09ece
Daniil <eo18622@yandex.ru>
2023-04-14 15:56:13 +0300

    стили для планшета 1

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,14 +1,11 @@
 const burgerMenu = document.querySelector('.header__burger');
 const navNode = document.querySelector('.header__menu');
 const pageBody = document.querySelector('.page__body');
-// const navList = document.querySelector('.nav__list');
-
 
 function toggleMenu() {
     console.log(burgerMenu);
     navNode.classList.toggle('header__menu-open');
     burgerMenu.classList.toggle('header__menu-burger-open');
-    // navList.classList.toggle('nav__list-open');
 
     // Флаг на открытие меню
     const isOpen = navNode.className.includes('header__menu-open');
